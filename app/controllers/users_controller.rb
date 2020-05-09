5a09b3dc5e3562038e41b26308a1a6d459d21888
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-05-09 01:04:05 +0000

    users controller draft

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,17 +23,11 @@ class UsersController < ApplicationController
   def show 
     @user = User.find_by_id(params[:id])
   end 
-      
-  
-  
   
   private 
   
-  
   def user_params
-    
+    params.require(:user).permit(:name, :email, :password)
   end 
-  
-  
   
 end
