49c0d6666a198def6de8cec808d259b493b930e3
theouternet <jrougeau@gmail.com>
2020-05-15 15:05:32 -0500

    helper last updated

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+    def current_user_path
+        if session[:user_id]
+          user_path(session[:user_id])
+        else
+          root_path
+        end
+      end
+    
+      def login_with_google
+       link_to image_tag('home/google_signin.png'), '/auth/google'
+      end
+
 end
